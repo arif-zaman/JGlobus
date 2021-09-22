877bd7df7edbe0765d8df4dafc9e2abcacbdbc54
Md Arifuzzaman <arif@nevada.unr.edu>
2021-09-22 13:29:43 -0700

    updated config

--- AdaptiveGridFTPClient/auto_collect.py
+++ AdaptiveGridFTPClient/auto_collect.py
@@ -2,8 +2,8 @@ import os
 
 #src = "gsiftp://oasis-dm.sdsc.xsede.org//oasis/scratch-comet/earslan/temp_project/1G-"
 base_format = "timeout 65 mvn exec:java -Dexec.args=\"src/main/resources/config.cfg -s {0} -d {1} -maxcc {2}\""
-src_format = "ftp://<source_ip>:50505/data/arif/{0}/"
-dest_format = "ftp://<dest_ip>:50505/data/arif/{0}/"
+src_format = "gsiftp://gridftp.stampede2.tacc.xsede.org:2811/scratch/06979/mazaman/data/{0}/"
+dest_format = "gsiftp://oasis-dm.sdsc.xsede.org/expanse/lustre/scratch/mazaman/temp_project/data/"
 ccs = [2,4,8,16,32]
 filesizes = ["large", "medium", "small"]
 
@@ -13,7 +13,7 @@ while True:
     for cc in ccs:
         for filesize in filesizes:
             src = src_format.format(filesize)
-            dest = dest_format.format(filesize)
+            dest = dest_format
             command = base_format.format(src,dest,cc)
             print(count, command)
             data = os.popen(command).read()
